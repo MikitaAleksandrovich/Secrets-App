cb794aff65b0167c4d1efd951959ba14296c9e50
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-07-02 22:56:36 +0400

    Create UserDB and save each user to newUser in database
    Only if user registered redirect him to secrets page.
    Also check if on login page user insert right login and pasword that is mathces to one in database, only in that case redirect him to secrets page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model('User', userSchema);
 
 
 app.get('/', (req, res) => {
@@ -23,6 +33,41 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    
+    newUser.save(err => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne(
+        {email: username, password: password},
+        (err, foundUser) => {
+            if(err) {
+                console.log(err);
+            } else {
+                if(foundUser) {
+                    if(foundUser.password === password) {
+                        res.render('secrets');
+                    }
+                }
+            }
+        }
+    )
 });
 
 
